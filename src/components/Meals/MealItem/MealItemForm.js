f508683fb8e7e32f3341c31641520b58ddb2443c
VladimirPariy <Vladimir.pariy@gmail.com>
2022-07-03 00:44:52 +0300

    created input component, edit mealItemForm and MealItem

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import cl from './MealItemForm.module.css'
+import Input from "../../UI/input/Input";
 
-const MealItemForm = () => {
+const MealItemForm = (props) => {
   return (
-    <form>
-      
+    <form className={cl.form}>
+      <Input label='Количество' input={{
+        id:'amount',
+        type:'number',
+        min:'0',
+        step:'1',
+        defaultValue:'0'
+      }}/>
+      <button>Добавить</button>
     </form>
   );
 };
