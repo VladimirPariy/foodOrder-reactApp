71b48058d090fcbe9338afc463f96fb61c87d0a1
VladimirPariy <Vladimir.pariy@gmail.com>
2022-07-03 00:26:40 +0300

    created card, mealItemForm

--- src/components/Meals/MealsList.js
+++ src/components/Meals/MealsList.js
@@ -2,9 +2,13 @@ import React from 'react';
 import {DUMMY_MEALS} from './dummy-meals'
 import cl from './MealList.module.css'
 import Card from "../UI/card/Card";
+import MealItem from "./MealItem/MealItem";
 
 const MealsList = () => {
-  const mealList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>)
+  const mealList = DUMMY_MEALS.map(meal => <MealItem name={meal.name}
+                                                     description={meal.description}
+                                                     price={meal.price}
+                                                     key={meal.id}/>)
   return (
     <section className={cl.meals}>
       <Card>
