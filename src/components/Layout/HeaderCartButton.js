ae53f7adee8ff83526243369592858b53711a9af
VladimirPariy <Vladimir.pariy@gmail.com>
2022-07-03 23:20:31 +0300

    used context for badge

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import cl from './HeaderCartButton.module.css'
 import CartIcon from "../Cart/CartIcon";
+import {useContext} from "react";
+import CartContext from "../../store/cart-context";
+import cartContext from "../../store/cart-context";
+
 
 const HeaderCartButton = (props) => {
+  
+  const context = useContext(CartContext)
+  const cartItemNumber = context.items.reduce((acc, rec) => {
+    return acc+rec.amount
+  }, 0)
+  
   return (
     <button className={cl.button} onClick={props.onClick}>
       <span className={cl.icon}>
         <CartIcon/>
       </span>
       <span>Корзина</span>
-      <span className={cl.badge}>2</span>
+      <span className={cl.badge}>{cartItemNumber}</span>
     </button>
   );
 };
